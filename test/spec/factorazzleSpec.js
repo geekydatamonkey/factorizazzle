16f6806a3f94020ee45c4da4c8ef1e68c6e8cb2e
James Johnson <johnsojr@gmail.com>
2014-09-17 22:39:49 -0500

    refactored factoring algorithm. speedier version using least common factor.

--- test/spec/factorazzleSpec.js
+++ test/spec/factorazzleSpec.js
@@ -7,21 +7,24 @@
         describe('generates prime factors', function () {
 
             it('should only take positive integers', function () {
+
+              var message = 'Not an integer > 1';
+
               expect(function(){ 
                 FACTORAZZLE.generate('Forty-Two');
-              }).toThrow(new Error('Number is not a positive integer'));
+              }).toThrow(new Error(message));
 
               expect(function(){
                 FACTORAZZLE.generate(-1);
-              }).toThrow(new Error('Number is not a positive integer'));
+              }).toThrow(new Error(message));
 
               expect(function(){
                 FACTORAZZLE.generate(2.5);
-              }).toThrow(new Error('Number is not a positive integer'));
+              }).toThrow(new Error(message));
 
               expect(function(){
                 FACTORAZZLE.generate(NaN);
-              }).toThrow(new Error('Number is not a positive integer'));
+              }).toThrow(new Error(message));
             });
 
             it('returns 2 for 2', function(){
@@ -32,6 +35,9 @@
             });
             it('returns [2,2] for 4', function(){
               expect(FACTORAZZLE.generate(4)).toEqual([2,2]);
+            });
+            it('returns [2,2,3,3] for 36', function(){
+              expect(FACTORAZZLE.generate(36)).toEqual([2,2,3,3]);
             });
             it('returns [2,2,3,5] for 60', function(){
               expect(FACTORAZZLE.generate(60)).toEqual([2,2,3,5]);
